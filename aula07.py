db687eb4da7441196e706143618afb30c63cdc0c
gabjohann <gabif.johann@gmail.com>
2021-06-11 00:11:51 -0300

    aulas

---
+++ aula07.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+# print('A soma vale {}'.format(n1 + n2))
+s = n1 + n2
+sub = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, a subtração é {}, a multiplicação é {} e a divisão é {}'.format(s, sub, m, d))
+print('A divisão inteira é {} e a potência é {}'.format(di, e))
+
+# para formatar utilizase dentro das {} a formtação, ex.: 3 casas decimais {:.3f}
